0a2701d9361aa2437bb31089bb5dbff0d6a4334d
tinhpv <tinhpv@nal.vn>
2022-09-12 18:20:40 +0700

    [PCA-46] Start timer at phone call screen

--- src/store/modules/twilio/mutations.js
+++ src/store/modules/twilio/mutations.js
@@ -2,9 +2,11 @@ const mutations = {
   setConnection(state, connection) {
     state.connection = connection;
   },
+
   setDevice(state, device) {
     state.device = device;
   },
+
   setIsShowCallTypeModal(state, value) {
     state.isShowCallTypeModal = value;
   },
