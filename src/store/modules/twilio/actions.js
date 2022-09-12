0b1e93c7ed1ea4b1dc38965a44a523c1efc93b41
Truong Hong Thai <thaith@nal.vn>
2022-09-12 16:05:06 +0700

    [PCA-47] Implement end call in Phone Call page

--- src/store/modules/twilio/actions.js
+++ src/store/modules/twilio/actions.js
@@ -1,11 +1,13 @@
 const actions = {
   disconnectCall({ state, commit }) {
     state.connection?.disconnect();
+    commit('setIsShowCallTypeModal', false);
     commit('setConnection', null);
   },
 
   rejectCall({ state, commit }) {
     state.connection?.reject();
+    commit('setIsShowCallTypeModal', false);
     commit('setConnection', null);
   },
 };
